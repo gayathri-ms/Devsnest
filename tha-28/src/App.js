6a843b1dc6844aa5203681e76bfe907b1522efa4
gayathri-ms <b219024@iiit-bh.ac.in.com>
2021-08-05 23:38:07 +0530

    Added THA-28

--- tha-28/src/App.js
+++ tha-28/src/App.js
@@ -4,27 +4,32 @@ import { useEffect , useState } from "react"
 function App() {
 
   const [input , setInput] = useState("")
-  const [place , setPlace] = useState("")
   const [totaldata ,setTotaldata] = useState({})
 
-  useEffect(() => {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=6d8af3f1434344cb96a163106210508&q=${place}`)
+  const place = () => {
+     fetch(`https://api.weatherapi.com/v1/current.json?key=6d8af3f1434344cb96a163106210508&q=${input}`)
     .then(res => res.json())
     .then(data => setTotaldata(data))
-  }, [] )
 
+    setInput("")
+  }
+   
+ 
+
+  console.log(totaldata)
   return (
     <div className="App">
       <input type="text" value={input} placeholder="Enter any city name" onChange={(e) => setInput(e.target.value) } />
-      <button onClick={() => {
-        setPlace(input);
-        setInput("")
-      }}
+      <button onClick={place}
       >Get Report</button>
       {
-         place ?
+         totaldata.location ?
           <div>
-            hello
+            <h1>{totaldata.location.name}</h1>
+            <img src={totaldata.current.condition.icon} alt="" />
+            <h3>Humidity: {totaldata.current.humidity}</h3>
+            <h3>Temperature: {totaldata.current.temp_c}</h3>
+            <h3>Wind speed: {totaldata.current.wind_kph}</h3>
           </div>
           : <h1> City not found </h1>
       }
