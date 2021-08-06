9001f584387a1b1354e2e04e48599d9da16291ed
gayathri-ms <b219024@iiit-bh.ac.in.com>
2021-08-06 22:35:57 +0530

    Updated THA-28

--- tha-28/src/App.js
+++ tha-28/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [totaldata ,setTotaldata] = useState({})
 
   const place = () => {
-     fetch(`https://api.weatherapi.com/v1/current.json?key=6d8af3f1434344cb96a163106210508&q=${input}`)
+     fetch(`https://api.weatherapi.com/v1/forecast.json?key=6d8af3f1434344cb96a163106210508&q=${input}`)
     .then(res => res.json())
     .then(data => setTotaldata(data))
 
@@ -19,17 +19,44 @@ function App() {
   console.log(totaldata)
   return (
     <div className="App">
-      <input type="text" value={input} placeholder="Enter any city name" onChange={(e) => setInput(e.target.value) } />
-      <button onClick={place}
+      <input className="input" type="text" value={input} placeholder="Enter any city name" onChange={(e) => setInput(e.target.value) } />
+      <button className="in_button" onClick={place}
       >Get Report</button>
       {
          totaldata.location ?
-          <div>
-            <h1>{totaldata.location.name}</h1>
-            <img src={totaldata.current.condition.icon} alt="" />
-            <h3>Humidity: {totaldata.current.humidity}</h3>
-            <h3>Temperature: {totaldata.current.temp_c}</h3>
-            <h3>Wind speed: {totaldata.current.wind_kph}</h3>
+         <div className="report" >
+          <div className="bg_img"></div>
+          <div className="forecast" >
+          <div style={{display:"flex" , marginBottom:"-10px"}} >
+             <div className="left" >
+              <h2>{totaldata.location.name}</h2>
+              <p style={{marginTop:"-5px" ,  fontSize:"15px" }}>{totaldata.location.localtime}</p>
+               <img style={{marginTop:"-5px" }} src={totaldata.current.condition.icon} alt="" />
+               <p style={{marginTop:"-5px" , fontSize:"20px" }}>{totaldata.current.condition.text}</p>
+            </div>
+            <div className="right" >
+               <h1 style={{fontSize:"60px"}} >{totaldata.current.temp_c}°C </h1>
+                <div>
+                  <span>{totaldata.forecast.forecastday[0].day.maxtemp_c}°C/</span>
+                  <span>{totaldata.forecast.forecastday[0].day.mintemp_c}°C</span>
+               </div>
+            </div>
+          </div>
+          
+          <p style={{fontSize:"20px" , marginTop:"0px"}}>
+            <span >Humidity: {totaldata.current.humidity}%</span>&nbsp;&nbsp;&nbsp;&nbsp;
+            <span >Wind speed: {totaldata.current.wind_kph}kmph</span>
+          </p>
+          <hr/>
+          <div style={{display:"flex"}} >
+            {/*<p><img src={totaldata.forecast.forecastday[0].hour[1].condition.icon} alt="" /></p>*/}
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+          </div>
+          </div>
           </div>
           : <h1> City not found </h1>
       }
